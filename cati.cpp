c6b7cd8f464fee90e9b464e4ad505f5115ae9197
eLite <141034305+eLiteEs@users.noreply.github.com>
2024-02-16 15:13:12 +0100

    Add files via upload

--- cati.cpp
+++ cati.cpp
@@ -3,6 +3,7 @@
 #include <windows.h> // for Sleep function
 #include <stdlib.h> // for rand function
 #include <vector> // for std::vector
+#include <thread> // for std::thread
 
 // Function to set cursor position in console
 void gotoxy(int x, int y) {
@@ -47,6 +48,20 @@ bool isColliding(int x1, int y1, int width1, int height1, int x2, int y2, int wi
         y1 + height1 > y2);
 }
 
+// Function to play a note of given frequency and duration asynchronously with a 1 millisecond gap
+void playNote(int frequency, int duration) {
+    // Use std::thread to run Beep() in a separate thread
+    std::thread soundThread([=]() {
+        Beep(frequency, duration);
+    });
+
+    // Detach the thread to allow it to run independently
+    soundThread.detach();
+
+    // Add a 1 millisecond delay before returning
+    std::this_thread::sleep_for(std::chrono::milliseconds(duration + 1));
+}
+
 // Function to set console background color
 void setBackgroundColor() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -77,147 +92,183 @@ int main() {
 
     // Score and lives
     int score = 0;
-    float lives = 9.5f; // Initial lives with .5 for testing
+    float lives = 3.0f; // Initial lives with .5 for testing
 
     // Vector to store arrows
     std::vector<std::pair<int, int>> arrows;
+    std::vector<std::pair<int, int>> hearts;
+
+    clearScreen();
+    // Draw white square with black borders
+     int squareWidth = 44;
+     int squareHeight = 10;
+     int squareX = (80 - squareWidth) / 2;
+     int squareY = (25 - squareHeight) / 2;
+    for (int y = squareY; y < squareY + squareHeight; ++y) {
+        gotoxy(squareX, y);
+        if (y == squareY || y == squareY + squareHeight - 1) {
+            for (int x = 0; x < squareWidth; ++x) {
+                
+            }
+        } else {
+            gotoxy(squareX, y);
+            std::cout << ASCII_BG_WHITE << ASCII_REVERSE << " " << ASCII_RESET;
+            gotoxy(squareX + squareWidth - 1, y);
+            std::cout << ASCII_BG_WHITE << ASCII_REVERSE << " " << ASCII_RESET;
+        }
+    }
+
+    // Print "Game Over" in the middle of the square
+    gotoxy(squareX + (squareWidth - 9) / 2, squareY + squareHeight / 2 - 3);
+    std::cout << ASCII_WHITE << ASCII_BOLD << ASCII_UNDERLINE << ASCII_BLINK << " Controls " << ASCII_RESET;
+
+    // Print "Press any key to exit" at the bottom
+    gotoxy(30, squareY + squareHeight / 2 - 2);
+    std::cout << "W -> up A -> left"; // Print "Press any key to exit" at the bottom
+    gotoxy(30, squareY + squareHeight / 2 - 1);
+    std::cout << "S -> down D -> right";
+     // Print "Press any key to exit" at the bottom
+    gotoxy(30, squareY + squareHeight / 2);
+    std::cout << "Spacebar -> Teleport Q -> exit";
+    gotoxy(30, squareY + squareHeight / 2 + 1);
+    std::cout << "P -> Pause";
+    gotoxy(30, squareY + squareHeight / 2 + 2);
+    std::cout << "Press any key to play";
+
+    // Wait for a key press to exit
+    _getch();
+
+    // Clear screen after exiting
+    clearScreen();
+
+    clearScreen();
+    bool paused = false;
 
     // Game loop
     while (lives > 0) {
-        clearScreen();
+        if (!paused) {
+            clearScreen();
 
-        // Display score
-        std::cout << ASCII_BOLD << "Score: " << ASCII_RESET << score << std::endl;
+            // Display score
+            std::cout << ASCII_BOLD << "Score: " << ASCII_RESET << score << std::endl;
 
-        // Display lives
-        gotoxy(80, 0);
-        std::cout << "Lives: ";
-        int numHearts = std::min(13, static_cast<int>(lives)); // Limit hearts to 13
-        for (int i = 0; i < numHearts; ++i) {
-            if (i == numHearts - 1 && lives - static_cast<int>(lives) == 0.5) {
-                std::cout << ASCII_BLINK; // Make the last heart blink if lives end with .5
+            // Display lives
+            gotoxy(80, 0);
+            std::cout << "Lives: ";
+            int numHearts = std::min(13, static_cast<int>(lives)); // Limit hearts to 13
+            for (int i = 0; i < numHearts; ++i) {
+                if (i == numHearts - 1 && lives - static_cast<int>(lives) == 0.5) {
+                    std::cout << ASCII_BLINK; // Make the last heart blink if lives end with .5
+                }
+                std::cout << ASCII_RED << (char)3 << ASCII_RESET; // Heart symbol
             }
-            std::cout << ASCII_RED << (char)3 << ASCII_RESET; // Heart symbol
-        }
 
-        // Display cat
-        setBackgroundColor();
-        for (int i = 0; i < numRows; ++i) {
-            gotoxy(catX, catY + i);
-            std::cout << cat[i];
-        }
-
-        // Display keys
-        gotoxy(70, 5);
-        std::cout << "_ w" << std::endl;
-        gotoxy(70, 6);
-        std::cout << "a s d";
-
-        // Display fruit
-        gotoxy(fruitX, fruitY);
-        std::cout << ASCII_GREEN << (char)5;
-        setBackgroundColor();
-
-        // Display arrows
-        if (rand() % 5 == 0) { // Decreased arrow frequency
-            arrows.push_back({ 70, rand() % 20 });
-        }
-        for (int i = 0; i < arrows.size(); ++i) {
-            gotoxy(arrows[i].first, arrows[i].second);
-            std::cout << (char)17; // Arrow symbol
-            arrows[i].first--; // Move arrow to the left
-            if (isColliding(catX, catY, 7, numRows, arrows[i].first, arrows[i].second, 1, 1)) {
-                lives -= 1.0f; // Decrease lives if arrow hits the cat
-                arrows.erase(arrows.begin() + i); // Remove arrow
+            // Display cat
+            setBackgroundColor();
+            for (int i = 0; i < numRows; ++i) {
+                gotoxy(catX, catY + i);
+                std::cout << cat[i];
             }
-            if (arrows[i].first <= 0) {
-                arrows.erase(arrows.begin() + i); // Remove arrow if it goes out of bounds
+
+
+            // Display fruit
+            gotoxy(fruitX, fruitY);
+            std::cout << ASCII_GREEN << (char)5;
+            setBackgroundColor();
+
+            // Display arrows
+            if (score < 20 && rand() % 5 == 0) { // Decreased arrow frequency
+                arrows.push_back({ 70, rand() % 20 });
             }
-        }
-
-        // Check collision with fruit
-        if (isColliding(catX, catY, 7, numRows, fruitX, fruitY, 1, 1)) {
-            score++; // Increase score if cat eats a fruit
-            lives += 0.5f; // Increase lives if cat eats a fruit
-            fruitX = rand() % 60; // Move fruit to a new random position
-            fruitY = rand() % 20;
-        }
-
-        // Get user input
-        if (_kbhit()) {
-            char key = _getch();
-            switch (key) {
-            case 'w':
-                if (catY > 0) catY--;
-                gotoxy(70, 5);
-                std::cout << "_ " << ASCII_REVERSE << "w" << ASCII_RESET << std::endl;
-                setBackgroundColor();
-                gotoxy(70, 6);
-                std::cout << "a s d";
-
-                break;
-            case 's':
-                if (catY < 20 - numRows) catY++;
-                gotoxy(70, 5);
-                std::cout << "_ w" << ASCII_RESET << std::endl;
-                setBackgroundColor();
-                gotoxy(70, 6);
-                std::cout << "a " << ASCII_REVERSE << "s" << ASCII_RESET;
-                setBackgroundColor();
-                std::cout << " d";
-                break;
-            case 'a':
-                if (catX > 0) catX--;
-                gotoxy(70, 5);
-                std::cout << "_ " << "w" << std::endl;
-                gotoxy(70, 6);
-                std::cout << ASCII_REVERSE << "a" << ASCII_RESET;
-                setBackgroundColor();
-                std::cout << " s d";
-                break;
-            case 'd':
-                if (catX < 60 - 7) catX++;
-                gotoxy(70, 5);
-                std::cout << "_ " << "w" << std::endl;
-                gotoxy(70, 6);
-                std::cout << "a s " << ASCII_REVERSE << "d" << ASCII_RESET;
-                setBackgroundColor();
-                break;
-            case ' ':
-                // Teleport cat to a random position
-                catX = rand() % 60;
-                catY = rand() % (20 - numRows);
-                lives -= 0.25f; // Decrease lives by 0.25 when teleporting
-                gotoxy(70, 5);
-                std::cout << ASCII_REVERSE << "_" << ASCII_RESET;
-                setBackgroundColor();
-                std::cout << " w" << std::endl;
-                gotoxy(70, 6);
-                std::cout << "a s " << "d" << ASCII_RESET;
-                setBackgroundColor();
-                break;
-            case 'q':
-                exit(0);
+            for (int i = 0; i < arrows.size(); ++i) {
+                gotoxy(arrows[i].first, arrows[i].second);
+                std::cout << (char)17; // Arrow symbol
+                arrows[i].first--; // Move arrow to the left
+                if (isColliding(catX, catY, 7, numRows, arrows[i].first, arrows[i].second, 1, 1)) {
+                    lives -= 1.0f; // Decrease lives if arrow hits the cat
+                    arrows.erase(arrows.begin() + i); // Remove arrow
+                }
+                if (arrows[i].first <= 0) {
+                    arrows.erase(arrows.begin() + i); // Remove arrow if it goes out of bounds
+                }
             }
-        }
 
-        // Wait a short time before next frame
-        Sleep(100);
+            // Display hearts
+            if (score < 20 && rand() % 25 == 0) { // Decreased heart frequency
+                hearts.push_back({ 70, rand() % 20 });
+            }
+            for (int i = 0; i < hearts.size(); ++i) {
+                gotoxy(hearts[i].first, hearts[i].second);
+                std::cout << ASCII_RED << (char)3 << ASCII_RESET; // Heart symbol
+                hearts[i].first--; // Move heart to the left
+                if (isColliding(catX, catY, 7, numRows, hearts[i].first, hearts[i].second, 1, 1)) {
+                    lives += 1.5f; // Increase lives if heart is collected by the cat
+                    hearts.erase(hearts.begin() + i); // Remove heart
+                }
+                if (hearts[i].first <= 0) {
+                    hearts.erase(hearts.begin() + i); // Remove heart if it goes out of bounds
+                }
+            }
+
+            // Check collision with fruit
+            if (isColliding(catX, catY, 7, numRows, fruitX, fruitY, 1, 1)) {
+                score++; // Increase score if cat eats a fruit
+                lives += 0.5f; // Increase lives if cat eats a fruit
+                fruitX = rand() % 53; // Move fruit to a new random position
+                fruitY = rand() % 17;
+            }
+
+
+            // Control boss and cat interaction
+            if (_kbhit()) {
+                char key = _getch();
+                if (key == 'w') {
+                    if (catY > 0) catY--;
+                } else if (key == 's') {
+                    if (catY < 20 - numRows) catY++;
+                } else if (key == 'a') {
+                    if (catX > 0) catX--;
+                } else if (key == 'd') {
+                    if (catX < 60 - 7) catX++;
+                } else if (key == ' ') {
+                    // Teleport cat to a random position
+                    catX = rand() % 60;
+                    catY = rand() % (20 - numRows);
+                    lives -= 0.25f; // Decrease lives by 0.25 when teleporting
+                } else if (key == 'q') {
+                    exit(0);
+                } else if (key == 'p') {
+                    paused = true;
+                }
+            }
+
+           
+
+            // Wait a short time before next frame
+            Sleep(100);
+        } else {
+            if (_kbhit()) {
+                char key = _getch();
+                if (key == 'P') {
+                    paused = false;
+                }
+            }
+            Sleep(100);
+        }
     }
 
     // Game over screen
     clearScreen();
 
     // Draw white square with black borders
-    const int squareWidth = 40;
-    const int squareHeight = 10;
-    const int squareX = (80 - squareWidth) / 2;
-    const int squareY = (25 - squareHeight) / 2;
+     squareWidth = 44;
+     squareHeight = 10;
+     squareX = (80 - squareWidth) / 2;
+     squareY = (25 - squareHeight) / 2;
     for (int y = squareY; y < squareY + squareHeight; ++y) {
         gotoxy(squareX, y);
         if (y == squareY || y == squareY + squareHeight - 1) {
             for (int x = 0; x < squareWidth; ++x) {
-                
             }
         } else {
             gotoxy(squareX, y);
